d3d821573fd9cf235e070af6a784df0cbf2fb8b1
yanlong <scicodist@gmail.com>
2016-03-29 20:57:20 +0800

    init

---
+++ smth_spider/pipelines.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+import os
+import time
+from scrapy.exceptions import DropItem
+
+class SmthSpiderPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+
+class DuplicatesPipeline(object):
+
+    def __init__(self):
+        self.ids_seen = set()
+
+    def process_item(self, item, spider):
+        if item['id'] in self.ids_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.ids_seen.add(item['id'])
+            return item
+
+class MongoPipeline(object):
+
+    collection_name = 'user'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        # self.db[self.collection_name].insert(dict(item))
+        # print item
+        if not self.db[self.collection_name].find_one({'id':item['id']}):
+            self.db[self.collection_name].insert_one(item)
+            user=item['id']
+            os.system('bash smth_spider/spiders/send_invitation.sh '+user)
+            time.sleep(5)
+            return item
+        else:
+            raise DropItem("Duplicate item found: %s" % item)
---
+++ smth_spider/settings.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for smth_spider project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'smth_spider'
+
+SPIDER_MODULES = ['smth_spider.spiders']
+NEWSPIDER_MODULE = 'smth_spider.spiders'
+
+MONGO_URI='mongodb://127.0.0.1:27017'
+MONGO_DATABASE='smth'
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'smth_spider (+http://www.yourdomain.com)'
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS=32
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY=3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN=16
+#CONCURRENT_REQUESTS_PER_IP=16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED=False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED=False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+'Cookie': '_ga=GA1.2.316291097.1429852824; Hm_lvt_7734e0ab76401586c0e18dc2feefc01b=1441631966; bfd_g=9de2782bcb754fd70000689300906e3054c9fda8; tma=88525828.7971704.1429693672137.1449543590241.1449629553140.142; tmd=2533.88525828.7971704.1429693672137.; main[UTMPUSERID]=gomypig; main[UTMPKEY]=28696048; main[UTMPNUM]=12483; main[PASSWORD]=%2503d%2500a2%2540%2526%250D%2516%250923%251Bu%2509%252C%250D%2522%2507Vg%2523%2504%250E; main[XWJOKE]=hoho; Hm_lvt_9c7f4d9b7c00cb5aba2c637c64a41567=1457776174,1457915149,1458001000,1458957533; Hm_lpvt_9c7f4d9b7c00cb5aba2c637c64a41567=1459236059',
+}
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'smth_spider.middlewares.MyCustomSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'smth_spider.middlewares.MyCustomDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   'smth_spider.pipelines.DuplicatesPipeline': 300,
+   'smth_spider.pipelines.MongoPipeline': 500,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+# NOTE: AutoThrottle will honour the standard settings for concurrency and delay
+#AUTOTHROTTLE_ENABLED=True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY=5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY=60
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG=False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED=True
+#HTTPCACHE_EXPIRATION_SECS=0
+#HTTPCACHE_DIR='httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES=[]
+#HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
